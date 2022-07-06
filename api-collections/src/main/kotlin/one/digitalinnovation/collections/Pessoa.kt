ce5b3a5ac872f32e67e636e96e1106b092f8874a
Valéria do Valle <valeriaabvalle@gmail.com>
2022-07-05 21:33:50 -0300

    Atualizado para aplicar o modificador private set à propriedade cpf

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -3,6 +3,8 @@ package one.digitalinnovation.collections
 class Pessoa {
     var nome: String = "Valeria"
     var cpf: String = "123.456.789-10"
+    private set
+    //Modificador de visibilidade: private impede tanto a visualização quanto a reatribuição, private set impede apenas a reatribuição.
 }
 
 fun main(){
