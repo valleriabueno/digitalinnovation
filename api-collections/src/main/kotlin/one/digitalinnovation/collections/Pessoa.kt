7c4bb04d23164082387d50d67b57d18a531695d7
Valéria do Valle <valeriaabvalle@gmail.com>
2022-07-20 12:20:56 -0300

    Atualizado para abordar membros de classe

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -5,11 +5,15 @@ class Pessoa {
     var cpf: String = "123.456.789-10"
     private set
     //Modificador de visibilidade: private impede tanto a visualização quanto a reatribuição, private set impede apenas a reatribuição.
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val valeria = Pessoa()
 
-    println(valeria.nome)
-    println(valeria.cpf)
+    println(valeria.pessoaInfo())
+    //println(valeria.cpf)
 }
