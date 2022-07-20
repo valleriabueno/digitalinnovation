6517ee52ed93a38e808705f6e3936b8836ec543f
Valéria do Valle <valeriaabvalle@gmail.com>
2022-07-20 12:44:54 -0300

    Criada classe Conta para demonstrar o construtor primário

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Conta.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation.collections
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+    //construtor primário
+) {
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}
+
